9d38173c32bed19daae93b2889d32f3c8634ab80
Okasha <okashasameer00@gmail.com>
2026-09-09 18:23:44 +0500

    reWrie the code of ViewPractice.js

--- screens/ViewPractice.js
+++ screens/ViewPractice.js
@@ -1,24 +1,16 @@
 import { View,Text, Pressable } from 'react-native';
 
-export default function ViewPractice( { navigation } ) {
+export default function ViewPractice({ navigation }) {
   return (
-    <View className="flex-1 items-center justify-center bg-cyan-400 px-4 py-4">
-        <Pressable className="rounded-xl bg-black px-8 py-4">
-            <Text className="text-lg text-white">
-                Hello World
-            </Text>
-        </Pressable>
-
-        <Pressable className="mt-4 rounded-xl bg-black px-8 py-4" onPress={() => navigation.goBack()}>
-            <Text className="text-lg text-white">
-                Go back
-            </Text>
-        </Pressable>
-
-
-
-      
-
+    <View className="flex-1 items-center justify-center bg-gray-100">
+      <Text className="text-2xl font-bold text-gray-900">
+        Welcome to the View Practice Screen!
+      </Text>
+      <Pressable className="mt-4 rounded-xl bg-black px-8 py-4" onPress={() => navigation.goBack()}>
+        <Text className="text-lg text-white">
+          Go back
+        </Text>
+      </Pressable>
     </View>
   );
 }
